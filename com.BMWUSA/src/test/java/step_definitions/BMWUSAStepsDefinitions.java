0ed85340c2644241db9d4f125e4e8f24f98428a6
Sabreen <smostafa1998@gmail.com>
2021-10-07 21:37:17 -0400

    added BMW cases

--- com.BMWUSA/src/test/java/step_definitions/BMWUSAStepsDefinitions.java
+++ com.BMWUSA/src/test/java/step_definitions/BMWUSAStepsDefinitions.java
@@ -1,20 +1,26 @@
 package step_definitions;
 
 import base.BaseClass;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.testng.Assert;
+import pom.Homepage;
+import pom.ShoppingHomePage;
 
 
 public class BMWUSAStepsDefinitions extends BaseClass {
 
     SharedStepsUI sharedStepsUI;
+    Homepage homepage;
+    ShoppingHomePage shopping;
 
 
     public BMWUSAStepsDefinitions() {
         sharedStepsUI = new SharedStepsUI();
-
+        homepage = new Homepage();
+        shopping = new ShoppingHomePage();
     }
 
     // STEPS DEFINITIONS GO IN HERE (i.e. A METHOD FOR EACH STEP OF THE SCENARIO)
@@ -32,7 +38,45 @@ public class BMWUSAStepsDefinitions extends BaseClass {
     @Then("user navigates to the Homepage")
     public void userNavigatesToTheHomepage() {
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.bmwusa.com/");
-
     }
+
+    @When("user navigates to the the shopping tab")
+    public void userNavigatesToTheTheShoppingTab() {
+        homepage.navigateToShoppingPage();
+    }
+
+    @And("user clicks on the estimate payment button")
+    public void userClicksOnTheEstimatePaymentButton() {
+        clickOnElement(shopping.estimateButton);
+    }
+
+    @And("user selects from the dropdown menu for {string}")
+    public void userSelectsFromTheDropdownMenuFor(String car_series) {
+        clickOnElement(shopping.series);
+        dropdownSelectByValue(shopping.series, car_series);
+    }
+
+    @And("user selects from the dropdown menu and picks {string}")
+    public void userSelectsFromTheDropdownMenuAndPicks(String car_model) {
+        clickOnElement(shopping.model);
+        dropdownSelectByValue(shopping.model, car_model);
+    }
+
+    @And("users enters in their {string}")
+    public void usersEntersInTheir(String zipCode) {
+        addingKeyboardInput(shopping.inputZIP, zipCode);
+    }
+
+    @And("user clicks on the estimate button to see")
+    public void userClicksOnTheEstimateButtonToSee() {
+        clickOnElement(shopping.estimatePayments);
+    }
+
+    @Then("user receives the {string} of the car")
+    public void userReceivesTheOfTheCar(String year) {
+        waitForElementToBeVisible(shopping.confirmYear);
+        Assert.assertEquals(shopping.confirmYear.getText(), year);
+    }
+
 
 }
--- com.BMWUSA/src/test/resources/example.feature
+++ com.BMWUSA/src/test/resources/example.feature
@@ -14,3 +14,19 @@ Feature: As a user, I should be able to navigate to the Homepage
       | Playstation 5  |      Video Games & Consoles     |
       | Hand Sanitizer |        Health & Beauty          |
       | iPhone 13      |    Cell Phones & Accessories    |
+
+  @ShoppingPage
+  Scenario Outline: User should be able to navigate to the homepage
+    When user navigates to the the shopping tab
+    And user clicks on the estimate payment button
+    And user selects from the dropdown menu for "<car_series>"
+    And user selects from the dropdown menu and picks "<car_model>"
+    And users enters in their "<zip_code>"
+    And user clicks on the estimate button to see
+    Then user receives the "<year>" of the car
+
+    Examples:
+      | car_series |  car_model | zip_code| year|
+      | 2          |   222V     | 11377   | 2022|
+      | 7          |   227N     | 90011   | 2022|
+      | 4          |   224E     | 04005   | 2022|
